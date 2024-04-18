44837b30c00e799afd8af5c6076b3d87402c195a
Dylan Heard <dylan.heard.business@gmail.com>
2024-04-18 04:44:09 -0700

    Added a default get request

--- ReadSmart_Microservice.py
+++ ReadSmart_Microservice.py
@@ -9,5 +9,13 @@ app = Flask(__name__)
 app.register_blueprint(question_generator, url_prefix='/question_generator')
 app.register_blueprint(response_grader, url_prefix='/response_grader')
 
+@app.route('/', methods=['GET'])
+def home():
+    return "ReadSmart Microservice is running!"
+
+@app.route('/favicon.ico', methods=['GET'])
+def favicon():
+    return ""
+
 if __name__ == "__main__":
     app.run(port=5000)
